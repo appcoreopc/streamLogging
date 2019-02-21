87bda01956207d63fa1bfd896514c18bd303869b
jeremy woo <kepungmoin@hotmail.com>
2019-02-21 20:20:05 +1300

    added c# 7.2.

--- src/ClientApp/Program.cs
+++ src/ClientApp/Program.cs
@@ -8,19 +8,21 @@ namespace ClientApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
 
             Channel channel = new Channel("127.0.0.1:9999", ChannelCredentials.Insecure);
 
             var client = new StreamLogging.StreamLoggingClient(channel);  
-              
+                      
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
             CancellationToken token = cancellationTokenSource.Token;  
 
             var streamCall = client.Log(new CallOptions(null, null, token));
 
+            
             int x = 0; 
+
 
             foreach (var item in Enumerable.Range(1, 2000))
             {
@@ -28,12 +30,19 @@ namespace ClientApp
                 var logRequest = new LogRequest() { Id = x.ToString(), Content = "Log Content " + DateTime.Now.ToLongTimeString() };
 
                 // I think isssue with this.... need to sync something here ! 
-                streamCall.RequestStream.WriteAsync(logRequest);
+               await streamCall.RequestStream.WriteAsync(logRequest);
+            
             }
+        
+          
+
+            //var status = streamCall.GetStatus();
+            //Console.WriteLine("Status for this is "+ status.StatusCode.ToString());
+
 
             var responseFRomServer = streamCall.GetAwaiter().GetResult().Content;
             Console.WriteLine(responseFRomServer);
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
